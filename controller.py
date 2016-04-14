ad9650f4d4bfbae01a0e397a44853b0cbb2d7599
jaredbriskman <jared.briskman@students.olin.edu>
2016-04-14 13:12:24 -0400

    more controller fixes

--- controller.py
+++ controller.py
@@ -1,6 +1,7 @@
 from Queue import Queue
 import threading
 from threading import Thread
+import time
 import sys
 import framework
 import view
@@ -12,12 +13,22 @@ keystrokes = Queue()
 strings = Queue()
 
 def main():
+    """Idles waiting for keystrokes. If key is escape, puts "esc" on keystrokes queue,
+    which will initiate framework raising SystemExit. If key is enter, it should start a bluemix STT worker thread."""
     key = ord(getch())
     if key == 27: #ESC
         keystrokes.put("esc")
     elif key == 13: #Enter
         #TODO: Start a bluemix thread? Have to sort bluemix out first.
         #Probably a worker daemon though.
+        strings.put(bluemix())
+
+def bluemix():
+    """Should stream data through a bluemix session until enter keyup and
+    recieve results, then return the most probable text as a string"""
+    pass
+    #Bluemix returns a JSON object, with text as an element
+
 if __name__ == '__main__':
     #Threading targets
     def _frameworkstart():
@@ -33,6 +44,7 @@ if __name__ == '__main__':
     	global _running
     	while _running:
     		main()
+            time.sleep(.01)
 
     def _viewstart():
     	global _running
