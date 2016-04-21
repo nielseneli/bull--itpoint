c12a269ff46ea3c6f03c7f0d3f0a6eb2e80a93e8
margaretmcrawf <margaretmcrawf@gmail.com>
2016-04-21 02:21:51 -0400

    functionality between framework and view

--- framework.py
+++ framework.py
@@ -5,44 +5,65 @@ import view
 #from controller import strings, keystrokes
 import sys
 import requests
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
 
 strings = ['create title slide', 'the title of this slide is cool shit']
+keystrokes = ['not quit']
+
+slide = view.Slide_List()
 
 #functions for APIs and things
 def get_image_url(searchterm):
-	API_URL = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q='
-	+ searchterm.replace(' ', '%20')
-	r = requests.get(API_URL)
-	return json.loads(r.text)['responseData']['results']['unescapedUrl'] #the url for the first image
+    API_URL = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q='
+    + searchterm.replace(' ', '%20')
+    r = requests.get(API_URL)
+    return json.loads(r.text)['responseData']['results']['unescapedUrl'] #the url for the first image
 
 # loop continues checking for new strings until user keystrokes
+@app.route('/')
 def main():
-    for text in strings.get():
-#       the following section is for creating a new slide
+    global slide
+    if strings:
+        text = strings.pop(0)
+        #       the following section is for creating a new slide
         if 'create title slide' in text:
+            print 'hit'
             slide = view.Slide_Title() # make new title slide
-            slide.update()
+            return slide.update()
         elif 'create list slide' in text:
             slide = view.Slide_List()  # make new list slide
-            slide.update()
+            return slide.update()
 
-#	This section calls slide methods when certain phrases are found in the string
+    #   This section calls slide methods when certain phrases are found in the string
 
         if 'the title of this slide is' in text:
             title = text.split('the title of this slide is', 1)[1]
-            slide.title = title
-            slide.update()
+            slide.make_title(title)
+            return slide.update()
         elif 'point' in text:
             bullet = text.split('point', 1)[1]
             slide.add_item(bullet)
-            slide.update()
-        if 'add image of' in text:
-        	searchterm = text.split('add image of')[1]
-        	url = get_image_url(searchterm)
-        	slide.add_image(url)
-        	slide.update()
+            return slide.update()
+        elif 'add image of' in text:
+            searchterm = text.split('add image of')[1]
+            url = get_image_url(searchterm)
+            slide.add_image(url)
+            return slide.update()
+    
 
-#if the escape key is pressed, that will be sent to the keystroke queue which will quit the program.
-    for key in keystrokes.get():
-        if keystrokes.pop() == 'esc':
-            sys.quit
+    #if the escape key is pressed, that will be sent to the keystroke queue which will quit the program.
+        for key in keystrokes:
+            if keystrokes.pop() == 'esc':
+                sys.quit
+
+    return slide.update()
+if __name__ == '__main__':
+    # enables debugging mode, so you don't have to restart
+    # the server each time you change your code
+    app.run(debug=True)
+    while True:
+        main()
