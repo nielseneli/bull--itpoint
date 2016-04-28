ab7497bc83ea9be242337f5a46c37dc39a269822
nielsenlouise <louiseenielsen@gmail.com>
2016-04-28 15:01:08 -0400

    commiting partially working view with reveal.js and adding index.copy to the repo

--- view.py
+++ view.py
@@ -1,49 +1,68 @@
 """ Contains the View part of the project. Has a Slide class, as well as
     classes that inherit from that: Slide_List, Slide_Title, etcself.
-    This branch uses HackerSlides instead of Flask to make it look like an
-    actual presentation.
+    This branch is implementing reveal.js to render a presentation.
 """
-""" things to do to use reveal-md:
-curl -sL https://deb.nodesource.com/setup | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo npm install -g reveal-md
-"""
-
-
-def end_slide(filename):
-    filename.write('\n\n---\n\n')
-
-
-def add_title_slide(filename, title='', subtitle=''):
-    filename.write('# ' + title + '\n\n' + '### ' + subtitle)
-    end_slide(filename)
-
-
-def add_list_slide(filename, title='', items=None):
-    if items is None:
-        items = []
-    filename.write('## ' + title + '\n')
-    for item in items:
-        filename.write('\n- ' + item)
-    end_slide(filename)
 
 
 class SlideDeck(object):
-    """ One SlideDeck object is a github-flavored markdown document.
+    """ The SlideDeck object is the index.html file in the reveal.js-master
+    folder. When a new SlideDeck object is initialized, it copies index.copy in
+    the slAIdes folder. It is necessary to run SlideDeck.end when the
+    presentation is complete to save a copy of index.html to preserve the
+    slidedeck for later use.
     """
     def __init__(self, filename='presentation'):
+        """ Initializes the SlideDeck object by defining the filename for
+        eventually saving the presentation and writes index.html in the
+        reveal.js-master directory to begin the slideshow.
+        """
         self.filename = filename
-        self.deck = open(filename + '.md', 'w')
+        with open('reveal.js-master/index.html', 'w') as deck:
+            with open('index.copy', 'r') as f:
+                read_data = f.read()
+            deck.write(read_data)
+
+    def add_text(self, text):
+        """ Inserts text at the text-insert cue, which is always after the
+        previous slide. The text-insert cue is hardcoded into index.copy but
+        can be changed (is currently \t\t\t\t<!-- insert here pls -->\n)
+        """
+        with open('reveal.js-master/index.html', 'r+') as deck:
+            deck_lines = deck.readlines()
+            insert = deck_lines.index('\t\t\t\t<!-- insert here pls -->\n')
+            deck_lines.insert(insert, text)
+            deck.seek(0)
+            deck.write(''.join(deck_lines))
+            deck.truncate()
+
+    def add_slide(self, slide_text):
+        """ Adds a slide with the correct html formatting to be a markdown
+        slide. slide_text is just plain text; more formatting, specific to
+        types of slides, must be provided. Calls SlideDeck.add_text().
+        """
+        formatting = '\t\t\t\t<section data-markdown>\n\t\t\t\t\t<script type ="text/template">\n\t\t\t\t\t\t'
+        formatting2 = '\n\t\t\t\t\t</script>\n\t\t\t\t</section>\n'
+        self.add_text(formatting + slide_text + formatting2)
 
     def end(self):
-        self.deck.close
+        """ Saves the index.html file as self.filename + .html, currently in
+        the slAIdes folder instead of the reveal.js-master folder.
+        """
+        with open(self.filename + '.html', 'w') as new_file:
+            with open('reveal.js-master/index.html', 'r') as f:
+                index_contents = f.read()
+            new_file.write(index_contents)
 
-    def add_slide(self, text):
-        self.deck.write(text)
-        self.deck.write('\n\n---\n\n')
+    def add_text_to_slide(self, additional_text):
+        # TODO: Implement this.
+        """ Finds the insert cue, goes back two lines, then adds things.
+        """
+        pass
 
 
 class Slide(object):
+    """ I'm fairly certain this is completely unnecessary.
+    """
     """ Creates a Slide object. Has child classes Slide_List and Slide_Title.
     Theoretically, should never initialize this class -- always initialize a
     child class that is more specific for what kind of slide you need.
@@ -62,7 +81,8 @@ class Slide(object):
         return 'This slide has no content.'
 
 
-class Slide_List(Slide):
+class Slide_List(SlideDeck):
+    # TODO: Finish implementing this with reveal.js.
     """ Inherits from Slide class. Can make a title and a list of items.
     """
 
@@ -71,30 +91,37 @@ class Slide_List(Slide):
         May cause errors if given 'items' is not a list or 'title' is not a string.
         '''
         self.title = title
+        # sets items to an empty list if none are preset
         if items is None:
-            self.items = [] #sets items to an empty list if none are preset
+            self.items = []
+        # sets items if preset to that list
+        # ay cause errors if items is not a list
         else:
-            self.items = items #sets items if preset to that list. May cause errors if items is not a list.
+            self.items = items
 
     def update(self):
         """Formats the list of items with Flask so it looks like a list and
         outputs to webpage. Calls separate file: slide_list_template.html
         """
-        self.addtoDeck()
-        return render_template('slide_list_template.html', title=self.title, items=self.items)
+        items_str = '\n- '.join(self.items)
+        self.add_slide('# ' + self.title + '\n\n- ' + items_str)
+
 
     def make_list(self, items):
         """ Takes in list of items and sets it as items attribute.
         """
         self.items = items
+        self.update
 
     def add_item(self, new_item):
         """ Takes in new_items and adds it to self.items.
         """
         self.items.append(new_item)
+        self.update
 
 
 class Slide_Title(Slide):
+    # TODO: Implement this with reveal.js.
     """ Inherits from Slide class. Can make a title and a subtitle for a title
     slide.
     """
@@ -129,10 +156,8 @@ if __name__ == '__main__':
     # the server each time you change your code
     # app.run(debug=True)
 
-    test = SlideDeck('test')
-    test.add_slide('look it might work')
-    test.add_slide("fuckin' awesome stuff here this is look at it")
-    test.add_slide('### i made a thing yay \n\n preeettyyyyy maybe')
-    # add_title_slide(test, 'title', 'subtitle')
-    # add_list_slide(test, 'listy slide', ['look', 'i', 'made', 'a', 'list'])
-    SlideDeck.end(test)
+    trying = SlideDeck()
+    trying.add_slide('hey')
+    current = Slide_List(title='list slide pls', items=['um', 'sure', 'k','mybe'])
+    current.update()
+    # current.make_list(['hey', 'not a very good fish at all'])
